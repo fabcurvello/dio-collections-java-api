f4949c9a942be98cade4cead7d86538fd722e900
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-05-19 22:18:41 -0300

    update cont

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int cont = 1; cont <= 5; cont++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.println("i = " + cont);
         }
     }
 }
